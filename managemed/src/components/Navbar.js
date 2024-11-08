0fe9c12e945407fdf04c83a6300292b6aae79ac7
Cheșa David <140747134+ChesaDavid@users.noreply.github.com>
2024-11-08 19:53:39 +0200

    nu stiu de ce

--- managemed/src/components/Navbar.js
+++ managemed/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import './navbar.css';
 import React from 'react';
-
+import logo from '../assets/hospital.svg';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav>
+      <img src={logo} className="App-logo-nav" alt="logo" />
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/auth">LogIn</Link></li>
